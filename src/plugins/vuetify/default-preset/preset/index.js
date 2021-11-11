c50ee8b08e17ae76e37bd80cadbea2a85033e51e
Luis Murrugarra <luismurrugarra17@gmail.com>
2021-11-10 21:11:54 -0500

    Base Proyecto

---
+++ src/plugins/vuetify/default-preset/preset/index.js
@@ -0,0 +1,26 @@
+require('./overrides.scss')
+
+export default {
+  theme: {
+    themes: {
+      light: {
+        primary: '#0d293f',
+        accent: '#0d6efd',
+        secondary: '#8A8D93',
+        success: '#56CA00',
+        info: '#16B1FF',
+        warning: '#FFEB3B',
+        error: '#FF4C51',
+      },
+      dark: {
+        primary: '#0d293f ',
+        accent: '#0d6efd',
+        secondary: '#8A8D93',
+        success: '#56CA00',
+        info: '#16B1FF',
+        warning: '#FFEB3B',
+        error: '#FF4C51',
+      },
+    },
+  },
+}
