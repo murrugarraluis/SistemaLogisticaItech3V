0fc77960ce38cc796b695a4c45771ab15f6629f2
Luis Murrugarra <luismurrugarra17@gmail.com>
2021-12-22 14:20:51 -0500

    Inicio de Sesion

--- src/api.js
+++ src/api.js
@@ -10,121 +10,174 @@ async function getAll(url) {
   return items
 }
 async function register(url, json) {
-  let JSON = { }
-  await axios.post(url, json).then(result => {
-    const { data } = result.data
-    const { message } = result.data
-    const { status } = result
-    JSON = {
-      data,
-      message,
-      status,
-    }
-  }).catch(error => {
-    if (error.response) {
-      const { errors } = error.response.data
-      const { status } = error.response
+  let JSON = {}
+  await axios
+    .post(url, json)
+    .then(result => {
+      const { data } = result.data
+      const { message } = result.data
+      const { status } = result
       JSON = {
-        errors,
+        data,
+        message,
         status,
       }
-    }
-  })
+    })
+    .catch(error => {
+      if (error.response) {
+        const { errors } = error.response.data
+        const { status } = error.response
+        JSON = {
+          errors,
+          status,
+        }
+      }
+    })
 
   return JSON
 }
 async function update(url, json) {
-  let JSON = { }
-  await axios.put(url, json).then(result => {
-    const { data } = result.data
-    const { message } = result.data
-    const { status } = result
-    JSON = {
-      data,
-      message,
-      status,
-    }
-  }).catch(e => {
-    if (e.response) {
-      const { errors } = e.response.data
-      const { status } = e.response
+  let JSON = {}
+  await axios
+    .put(url, json)
+    .then(result => {
+      const { data } = result.data
+      const { message } = result.data
+      const { status } = result
       JSON = {
-        errors,
+        data,
+        message,
         status,
       }
-    }
-  })
+    })
+    .catch(e => {
+      if (e.response) {
+        const { errors } = e.response.data
+        const { status } = e.response
+        JSON = {
+          errors,
+          status,
+        }
+      }
+    })
 
   return JSON
 }
 async function destroy(url) {
-  let JSON = { }
-  await axios.delete(url).then(result => {
-    const { message } = result.data
-    const { status } = result
-    JSON = {
-      message,
-      status,
-    }
-  }).catch(e => {
-    if (e.response) {
-      const { errors } = e.response.data
-      const { status } = e.response
+  let JSON = {}
+  await axios
+    .delete(url)
+    .then(result => {
+      const { message } = result.data
+      const { status } = result
       JSON = {
-        errors,
+        message,
         status,
       }
-    }
-  })
+    })
+    .catch(e => {
+      if (e.response) {
+        const { errors } = e.response.data
+        const { status } = e.response
+        JSON = {
+          errors,
+          status,
+        }
+      }
+    })
 
   return JSON
 }
 async function getDeleted(url) {
-  let JSON = { }
-  await axios.get(url).then(result => {
-    const { data } = result.data
-    const { status } = result
-    JSON = {
-      data,
-      status,
-    }
-  }).catch(error => {
-    if (error.response) {
-      const { errors } = error.response.data
-      const { status } = error.response
+  let JSON = {}
+  await axios
+    .get(url)
+    .then(result => {
+      const { data } = result.data
+      const { status } = result
       JSON = {
-        errors,
+        data,
         status,
       }
-    }
-  })
+    })
+    .catch(error => {
+      if (error.response) {
+        const { errors } = error.response.data
+        const { status } = error.response
+        JSON = {
+          errors,
+          status,
+        }
+      }
+    })
 
   return JSON
 }
 async function restore(url) {
-  let JSON = { }
-  await axios.put(url).then(result => {
-    const { data } = result.data
-    const { message } = result.data
-    const { status } = result
-    JSON = {
-      data,
-      message,
-      status,
-    }
-  }).catch(e => {
-    if (e.response) {
-      const { errors } = e.response.data
-      const { status } = e.response
+  let JSON = {}
+  await axios
+    .put(url)
+    .then(result => {
+      const { data } = result.data
+      const { message } = result.data
+      const { status } = result
       JSON = {
-        errors,
+        data,
+        message,
         status,
       }
-    }
-  })
+    })
+    .catch(e => {
+      if (e.response) {
+        const { errors } = e.response.data
+        const { status } = e.response
+        JSON = {
+          errors,
+          status,
+        }
+      }
+    })
 
   return JSON
 }
+async function login(url, json) {
+  let JSON = {}
+  await axios
+    .post(url, json)
+    .then(result => {
+      const { data } = result.data
+      const { message } = result.data
+      const { status } = result
+      JSON = {
+        data,
+        message,
+        status,
+      }
+    })
+    .catch(error => {
+      if (error.response) {
+        // const { errors } = error.response.data
+        // const { status } = error.response
+        JSON = {
+          errors: error.response,
+        }
+      }
+    })
+
+  return JSON
+}
+async function csrf() {
+  const response = axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie')
+
+  return response
+}
 export default {
-  getAll, register, update, destroy, getDeleted, restore,
+  getAll,
+  register,
+  update,
+  destroy,
+  getDeleted,
+  restore,
+  login,
+  csrf,
 }
--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -4,7 +4,10 @@
       <v-card class="auth-card">
         <!-- logo -->
         <v-card-title class="d-flex align-center justify-center py-7">
-          <router-link to="/" class="d-flex align-center">
+          <router-link
+            to="/"
+            class="d-flex align-center"
+          >
             <v-img
               :src="require('@/assets/images/logos/logo.svg')"
               max-height="30px"
@@ -14,14 +17,20 @@
               class="me-3"
             ></v-img>
 
-            <h2 class="text-2xl font-weight-semibold">Materio</h2>
+            <h2 class="text-2xl font-weight-semibold">
+              Materio
+            </h2>
           </router-link>
         </v-card-title>
 
         <!-- title -->
         <v-card-text>
-          <p class="text-2xl font-weight-semibold text--primary mb-2">Welcome to Materio! 👋🏻</p>
-          <p class="mb-2">Please sign-in to your account and start the adventure</p>
+          <p class="text-2xl font-weight-semibold text--primary mb-2">
+            Welcome to Materio! 👋🏻
+          </p>
+          <p class="mb-2">
+            Please sign-in to your account and start the adventure
+          </p>
         </v-card-text>
 
         <!-- login form -->
@@ -48,20 +57,37 @@
             ></v-text-field>
 
             <div class="d-flex align-center justify-space-between flex-wrap">
-              <v-checkbox label="Remember Me" hide-details class="me-3 mt-1"> </v-checkbox>
+              <v-checkbox
+                label="Remember Me"
+                hide-details
+                class="me-3 mt-1"
+              >
+              </v-checkbox>
 
               <!-- forgot link -->
-              <a href="javascript:void(0)" class="mt-1"> Forgot Password? </a>
+              <a
+                href="javascript:void(0)"
+                class="mt-1"
+              > Forgot Password? </a>
             </div>
 
-            <v-btn block color="primary" class="mt-6" @click="onSubmit"> Login </v-btn>
+            <v-btn
+              block
+              color="primary"
+              class="mt-6"
+              @click="onSubmit"
+            >
+              Login
+            </v-btn>
           </v-form>
         </v-card-text>
 
         <!-- create new account  -->
         <v-card-text class="d-flex align-center justify-center flex-wrap mt-2">
           <span class="me-2"> New on our platform? </span>
-          <router-link :to="{ name: 'pages-register' }"> Create an account </router-link>
+          <router-link :to="{ name: 'pages-register' }">
+            Create an account
+          </router-link>
         </v-card-text>
 
         <!-- divider -->
@@ -73,7 +99,12 @@
 
         <!-- social links -->
         <v-card-actions class="d-flex justify-center">
-          <v-btn v-for="link in socialLink" :key="link.icon" icon class="ms-1">
+          <v-btn
+            v-for="link in socialLink"
+            :key="link.icon"
+            icon
+            class="ms-1"
+          >
             <v-icon :color="$vuetify.theme.dark ? link.colorInDark : link.color">
               {{ link.icon }}
             </v-icon>
@@ -90,10 +121,20 @@
     />
 
     <!-- tree -->
-    <v-img class="auth-tree" width="247" height="185" src="@/assets/images/misc/tree.png"></v-img>
+    <v-img
+      class="auth-tree"
+      width="247"
+      height="185"
+      src="@/assets/images/misc/tree.png"
+    ></v-img>
 
     <!-- tree  -->
-    <v-img class="auth-tree-3" width="377" height="289" src="@/assets/images/misc/tree-3.png"></v-img>
+    <v-img
+      class="auth-tree-3"
+      width="377"
+      height="289"
+      src="@/assets/images/misc/tree-3.png"
+    ></v-img>
   </div>
 </template>
 
@@ -101,6 +142,7 @@
 // eslint-disable-next-line object-curly-newline
 import { mdiFacebook, mdiTwitter, mdiGithub, mdiGoogle, mdiEyeOutline, mdiEyeOffOutline } from '@mdi/js'
 import { ref } from '@vue/composition-api'
+import api from '@/api'
 
 export default {
   setup() {
@@ -140,13 +182,26 @@ export default {
         mdiEyeOutline,
         mdiEyeOffOutline,
       },
+      editedItem: {
+        email,
+        password,
+      },
     }
   },
   methods: {
     async onSubmit() {
       try {
-        await this.$store.dispatch('doLogin', this.email)
-        this.$router.push({ name: 'dashboard' })
+        const url = `${this.$URL_SERVE}/login`
+        this.editedItem.email = this.email
+        this.editedItem.password = this.password
+        const data = this.editedItem
+        const response = await api.login(url, data)
+        if (response.status === 200) {
+          await this.$store.dispatch('doLogin', this.email)
+          this.$router.push({ name: 'dashboard' })
+        } else {
+          console.log(response.errors)
+        }
       } catch (error) {
         console.error(error)
       }
