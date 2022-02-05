9d23278b0682324e8c6254ee0ee2af6b55c6e681
Luis Murrugarra <luismurrugarra17@gmail.com>
2022-02-05 11:45:21 -0500

    Data para Materiales

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ import 'sweetalert2/dist/sweetalert2.css'
 
 Vue.config.productionTip = false
 
-Vue.prototype.$URL_SERVE = ' http://127.0.0.1:8000/api'
+Vue.prototype.$URL_SERVE = 'http://api-itech3v.test/api'
 
 const options = {
   confirmButtonColor: '#C62828',
